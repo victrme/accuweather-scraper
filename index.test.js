ca90b618471d812efc195314b879c4277e84c0a5
Victor Azevedo <victor.azevedo@mail.com>
2024-05-16 17:32:55 +0200

    Reordered test functions

--- index.test.js
+++ index.test.js
@@ -1,47 +1,15 @@
 import scraper from './index.js'
 
-const types = {
-	lat: 'string',
-	lon: 'string',
-	city: 'string',
-	region: 'string',
-	path: 'string',
-	now: {
-		icon: 'number',
-		temp: 'number',
-		feels: 'number',
-		description: 'string',
-	},
-	sun: {
-		duration: 'string',
-		rise: 'number',
-		set: 'number',
-	},
-	hourly: [
-		{
-			timestamp: 'number',
-			temp: 'number',
-			rain: 'string',
-		},
-	],
-	daily: [
-		{
-			timestamp: 'number',
-			high: 'number',
-			low: 'number',
-			day: 'string',
-			night: 'string',
-			rain: 'string',
-		},
-	],
-}
+await test()
 
 async function test() {
 	const url = 'https://example.com?lang=fr&lat=48.8582&lon=2.2944'
 	const resp = await scraper.fetch(new Request(url))
 	const json = await resp.json()
+	const types = scraperTypes()
 
 	compareTypes(json, types)
+	console.log('✅ Type test passed', '\n')
 }
 
 function compareTypes(obj, types) {
@@ -52,9 +20,45 @@ function compareTypes(obj, types) {
 		}
 
 		if (typeof obj[key] !== type) {
-			throw `${key} should be of type ${type}, but got ${typeof obj[key]}`
+			throw `"${key}" should be of type "${type}", but got "${typeof obj[key]}"`
 		}
 	}
 }
 
-await test()
+function scraperTypes() {
+	return {
+		lat: 'string',
+		lon: 'string',
+		city: 'string',
+		region: 'string',
+		link: 'string',
+		now: {
+			icon: 'number',
+			temp: 'number',
+			feels: 'number',
+			description: 'string',
+		},
+		sun: {
+			duration: 'string',
+			rise: 'number',
+			set: 'number',
+		},
+		hourly: [
+			{
+				timestamp: 'number',
+				temp: 'number',
+				rain: 'string',
+			},
+		],
+		daily: [
+			{
+				timestamp: 'number',
+				high: 'number',
+				low: 'number',
+				day: 'string',
+				night: 'string',
+				rain: 'string',
+			},
+		],
+	}
+}
